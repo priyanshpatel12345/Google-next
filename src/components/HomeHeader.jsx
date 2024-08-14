e54a9bf43dec4006dd3986d16c3353eb8545623d
Priyansh Patel <priyanshpatel741@gmail.com>
2024-08-15 00:50:14 +0530

    add HomeHeader components

---
+++ src/components/HomeHeader.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import { TbGridDots} from "react-icons/tb"
+
+export default function HomeHeader() {
+  return (
+    <header className="flex justify-end p-5">
+        <div className="flex space-x-4 items-center">
+            <Link href={"https://mail.google.com"}>
+            Gmail
+            </Link>
+            <Link href={"https://image.google.com"}>
+                Images
+            </Link>
+            <TbGridDots className="bg-transparent hover:bg-gray-200 text-4xl rounded-full p-2"/>
+            <button className="bg-blue-500 text-white px-5 py-2 rounded-md font-medium hover:brightness-110 shadow-md">
+                Sign in
+            </button>
+        </div>
+    </header>
+  );
+}
