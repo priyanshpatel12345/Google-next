4582acf2aac00a444ff01226441d14b6ca057f5e
Priyansh Patel <priyanshpatel741@gmail.com>
2024-08-15 18:43:52 +0530

    update search header components

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import SearchBox from "./SearchBox";
+import { RiSettings3Line } from "react-icons/ri";
+import { TbGridDots } from "react-icons/tb";
 
 export default function SearchHeader() {
-  return <div>SearchHeader</div>;
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            src="https://static-00.iconduck.com/assets.00/google-icon-2048x673-w3o7skkh.png"
+            width={120}
+            height={40}
+            priority
+            style={{ width: "auto" }}
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox />
+        </div>
+        <div className="hidden md:inline-flex space-x-2 ">
+          <RiSettings3Line className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
+          <TbGridDots className="bg-transparent hover:bg-gray-200 p-2 text-4xl rounded-full cursor-pointer" />
+        </div>
+        <button className="ml-2 bg-blue-500 text-white px-5 py-2 rounded-md font-medium hover:brightness-110 shadow-md">
+          Sign in
+        </button>
+      </div>
+    </header>
+  );
 }
